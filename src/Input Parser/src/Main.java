d1d7c2f3b881161c55e624d628923c12e7bf11c8
Kunal <sentythee@gmail.com>
2012-12-17 21:37:49 -0500

    minor code cleanup
    Remove the unused antlr files and the sequence package.
    Also remove the blank "getMostLikelyFirstChar" method.

--- src/Input Parser/src/Main.java
+++ src/Input Parser/src/Main.java
@@ -4,17 +4,20 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Agent agent = new Agent(10, 20);
+		
 		agent.train(new int[] {2, 3, 4, 9}, "asd ");
 		agent.train(new int[] {2, 3, 2, 9}, "xyz ");
 		agent.train(new int[] {6}, "q");
 		agent.addUncertainty(1);
 		
 		char[] res = agent.test(2);
-		res = agent.test(3);
-		res = agent.test(2);
-		res = agent.test(2);
-		res = agent.test(3);
+		agent.test(3);
+		agent.test(2);
+		agent.test(2);
+		agent.test(3);
 		res = agent.test(4);
+		
+		// should be "asdxyz" backwards
 		System.out.println(res[0]);
 		System.out.println(res[1]);
 		System.out.println(res[2]);
